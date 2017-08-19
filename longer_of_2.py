6c7827fb8847ff71e81cc44b98180d0d1b566278
AJ <jhaaashutoshkumar@yahoo.com>
2017-08-19 15:53:34 +0545

    fixed lint warnings

--- longer_of_2.py
+++ longer_of_2.py
@@ -1,17 +1,19 @@
 def longest(s1, s2):
     """[summary]
-    
-    [Take 2 strings s1 and s2 including only letters from ato z. 
-    Return a new sorted string, the longest possible, containing distinct letters,
+
+    [Take 2 strings s1 and s2 including only letters from ato z.
+    Return a new sorted string, the longest possible, containing distinct
+    letters,
     each taken only once - coming from s1 or s2. ]
-    
+
     Arguments:
         s1 {[string]} -- [description]
         s2 {[string]} -- [description]
-    each taken only once - coming from s1 or s2. 
-    #Examples:  a = "xyaabbbccccdefww" b = "xxxxyyyyabklmopq" 
+    each taken only once - coming from s1 or s2.
+    #Examples:  a = "xyaabbbccccdefww" b = "xxxxyyyyabklmopq"
     longest(a, b) -> "abcdefklmopqwxy"
-    a = "abcdefghijklmnopqrstuvwxyz" longest(a, a) -> "abcdefghijklmnopqrstuvwxyz" 
+    a = "abcdefghijklmnopqrstuvwxyz"
+    longest(a, a) -> "abcdefghijklmnopqrstuvwxyz"
 
     """
     return "".join(sorted(set(s1 + s2)))
