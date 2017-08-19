dfd28cb7337187c3d21a490beea87e4138d7e785
AJ <jhaaashutoshkumar@yahoo.com>
2017-08-19 16:34:52 +0545

    done, after around 30 minutes of own work

---
+++ binary_array_to_decimal.py
@@ -0,0 +1,29 @@
+def binary_array_to_number(arr):
+    """[summary]
+    Given an array of one's and zero's convert the equivalent binary
+    value to an integer.
+
+    Eg: [0, 0, 0, 1] is treated as 0001 which is the binary representation of 1
+
+    Examples:
+
+    Testing: [0, 0, 0, 1] ==> 1
+    Testing: [0, 0, 1, 0] ==> 2
+    Testing: [0, 1, 0, 1] ==> 5
+    Testing: [1, 0, 0, 1] ==> 9
+    Testing: [0, 0, 1, 0] ==> 2
+    Testing: [0, 1, 1, 0] ==> 6
+    Testing: [1, 1, 1, 1] ==> 15
+    Testing: [1, 0, 1, 1] ==> 11
+    """
+    arr_len = len(arr)
+    decimal = 0
+    for bit in arr:
+        print("bit = ", bit)
+        arr_len -= 1
+        if arr_len is (-1):
+            break
+
+        decimal += bit * (2 ** arr_len)
+
+    return decimal
